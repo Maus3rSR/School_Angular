2caf6b9c263ff0344742c9973e15f86b02473ef4
Kevin UNFRICHT <unfricht.kevin@hotmail.fr>
2026-03-11 14:43:20 +0100

    NEW (game-detail): bind game data from catalog + toggle favorite functionality

--- src/pages/game-detail/game-detail.component.ts
+++ src/pages/game-detail/game-detail.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { GameCatalog } from '../../features/game/game-catalog';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Game } from '../../features/game/game.model';
+import { FlixButton } from '../../ui/button/flix-button';
 
 @Component({
   selector: 'app-game-detail',
-  imports: [],
+  imports: [FlixButton, RouterLink],
   templateUrl: './game-detail.page.html',
   styleUrl: './game-detail.css',
 })
-export class GameDetail {}
+export class GameDetail {
+  private readonly route = inject(ActivatedRoute);
+  protected readonly catalog = inject(GameCatalog);
+
+  private readonly gameId = parseInt(this.route.snapshot.paramMap.get('id') ?? '');
+
+  protected game = computed<Game | undefined>(() => this.catalog.getGameSheet(this.gameId));
+}
